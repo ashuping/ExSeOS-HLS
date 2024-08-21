45d22439db53a9b03dae2e4ae6f8171c93bf1c8a
Alexis Maya-Isabelle Shuping <alexis@shuping.ai>
2024-08-21 14:53:15 -0500

    Start work on Variables and Constraints

---
+++ modules/data/Variable.py
@@ -0,0 +1,151 @@
+'''
+    Chicory ML Workflow Manager
+    Copyright (C) 2024  Alexis Maya-Isabelle Shuping
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+from modules.types.Option import Option, Nothing
+
+from typing import TypeVar
+from abc import ABC, abstractmethod
+
+A = TypeVar("A")
+
+class Variable[A](ABC):
+	''' Stores a quantity whose value can vary from workflow to workflow,
+		controlled either statically (manually set in configuration) or
+		dynamically (controlled by an optimizer)
+	'''
+	@property
+	@abstractmethod
+	def is_bound(self) -> bool:
+		''' `True` iff this variable has been bound to a value.
+
+			Note that `Variable.val` will only return a value if the variable is
+			bound; otherwise, a `TypeError` will be raised.
+		'''
+		... # pragma: no cover
+
+	@property
+	@abstractmethod
+	def name(self) -> str:
+		''' The name of this Variable.
+		'''
+		... # pragma: no cover
+
+	@property
+	@abstractmethod
+	def desc(self) -> Option[str]:
+		''' An optional long-form description for this Variable.
+		'''
+		... # pragma: no cover
+
+	@property
+	@abstractmethod
+	def val(self) -> A:
+		''' The bound value of this Variable. Only exists if `is_bound` is True;
+			otherwise, a `TypeError` will be raised.
+		'''
+		... # pragma: no cover
+
+	@property
+	@abstractmethod
+	def var_type(self) -> Option[type]:
+		''' Optional type annotation for this Variable.
+		'''
+		... # pragma: no cover
+
+	@property
+	@abstractmethod
+	def default(self) -> Option[A]:
+		''' Optional default value for this Variable.
+		'''
+		... # pragma: no cover
+
+	@abstractmethod
+	def bind(self, val: A) -> 'BoundVariable[A]':
+		''' Bind a value to this Variable. 
+		
+			This only works for unbound Variables; otherwise, a TypeError will
+			be raised.
+		'''
+		... # pragma: no cover
+
+class BoundVariable[A](Variable):
+	''' A Variable that has already been given a value.
+	'''
+	def __init__(self, name: str, val: A, var_type: Option[type] = Nothing(), desc: Option[str] = Nothing(), default: Option[A] = Nothing()):
+		self.__name = name
+		self.__val = val
+		self.__type = var_type
+		self.__desc = desc
+		self.__default = default
+
+	@property
+	def is_bound(self) -> bool:
+		return True
+
+	@property
+	def name(self) -> str:
+		return self.__name
+	
+	@property
+	def desc(self) -> Option[str]:
+		return self.__desc
+
+	@property
+	def val(self) -> A:
+		return self.__val
+
+	@property
+	def var_type(self) -> Option[type]:
+		return self.__type
+
+	@property
+	def default(self) -> Option[A]:
+		return self.__default
+
+
+class UnboundVariable[A](Variable):
+	''' A Variable which has not yet been given a value.
+	'''
+	def __init__(self, name: str, var_type: Option[type], desc: Option[str] = Nothing(), default: Option[A] = Nothing()):
+		self.__name    = name
+		self.__type    = var_type
+		self.__desc    = desc
+		self.__default = default
+
+	@property
+	def is_bound(self) -> bool:
+		return False
+
+	@property
+	def name(self) -> str:
+		return self.__name
+
+	@property
+	def desc(self) -> Option[str]:
+		return self.__desc
+
+	@property
+	def val(self) -> A:
+		raise TypeError("Tried to access `val` on an unbound variable!")
+
+	@property
+	def var_type(self) -> Option[type]:
+		return self.__type
+
+	@property
+	def default(self) -> Option[A]:
+		return self.__default
